e1777ff957636bd44acf738d08041843e5fe7479
Your Name Here <emercado@LibiMac02-16.local>
2014-05-19 13:57:28 -0700

    edited

--- collatz.js
+++ collatz.js
@@ -1,1 +1,15 @@
 console.log("edward's collatz");
+var collatz = function(x) {
+console.log("collatz");
+	while{
+	x > 1
+	}
+	if x % 2 ==0;{
+	x = (x / 2);
+	}
+	else{
+	x = (3*x + 1); 
+	}
+function print("\ncollatz(%d) has %d items" %){
+  console.log("collatz");
+}
